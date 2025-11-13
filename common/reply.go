b92b9c9bb7793a468b208afec06a81b6b940bb71
Steffen Schulze <steffen.schulze@t-systems.com>
2025-11-13 23:15:44 +0100

    subject builder integrated

--- common/reply.go
+++ common/reply.go
@@ -3,6 +3,7 @@ package common
 type Reply struct {
 	TenantId  string `json:"tenant_id"`
 	RequestId string `json:"request_id"`
+	GroupId   string `json:"group_id,omitempty"`
 	Error     *Error `json:"error"`
 }
 
--- common/request.go
+++ common/request.go
@@ -1,7 +1,24 @@
 package common
 
+import (
+	"crypto/sha256"
+	"encoding/hex"
+)
+
 type Request struct {
 	TenantId  string `json:"tenant_id"`
 	RequestId string `json:"request_id"`
 	GroupId   string `json:"group_id,omitempty"`
+}
+
+func (req *Request) BuildSubject() string {
+	s := req.TenantId + ";" + req.GroupId + ";" + req.RequestId + ";"
+
+	h := sha256.New()
+
+	h.Write([]byte(s))
+
+	bs := h.Sum(nil)
+
+	return hex.EncodeToString(bs)
 }
